54574b9443e0f966f72626321088f026f7457afa
spondbob <spondbob@eamca.com>
2016-09-11 14:44:22 +1000

    separate custom js into its own file

---
+++ my.js
@@ -0,0 +1,20 @@
+// Affix-like hack
+$(window).on('scroll', function(event) {
+  var windowWidth = $(window).width();
+  if (windowWidth >= 1000) {
+    var scrollValue = $(window).scrollTop();
+    if (scrollValue > 350) {
+      $('#sidebar-wrapper').css('top', scrollValue - 350); 
+    } else {
+      $('#sidebar-wrapper').css('top', 0); 
+    }
+  }
+});
+
+// Request to sa-hello API
+$().ready(function (){
+  $.getJSON( "https://spondbob.github.io/say-hello/api/en/hello.json", function( data ) {
+    var index = Math.floor((Math.random() * data.length));
+    $( "#greeting-message" ).html( data[index] + "," ).css("color", "").fadeIn(3000);
+  });
+});
