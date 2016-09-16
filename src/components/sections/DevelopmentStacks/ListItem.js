ad18b7782f83b6385a704c1ee6fad81064a1a4a9
spondbob <spondbob@eamca.com>
2016-09-16 14:45:46 +1000

    separate DevelopmentStacks into its own component

---
+++ src/components/sections/DevelopmentStacks/ListItem.js
@@ -0,0 +1,38 @@
+import React, { Component, PropTypes } from 'react';
+
+class ListItem extends Component {
+  handleOnClick(e, keys) {
+    e.preventDefault()
+    this.props.handleSetFilters(keys)
+  }
+
+  render() {
+    return (
+      <div className="list-group">
+        <div className="list-group-item list-group-item-action active">
+          <h5 className="list-group-item-heading">{this.props.data.title}</h5>
+          <p className="list-group-item-text">{this.props.data.description}</p>
+        </div>
+        {
+          this.props.data.items.map(function(item, key) {
+            return (
+              <a href="#"
+                key={key}
+                className="list-group-item list-group-item-action"
+                onClick={(event) => this.handleOnClick(event, item.keys)}>
+                { item.names.join(", ") }
+              </a>
+            )
+          }.bind(this))
+        }
+      </div>
+    )
+  }
+}
+
+ListItem.propTypes = {
+  data: PropTypes.object.isRequired,
+  handleSetFilters: PropTypes.func.isRequired
+}
+
+export default ListItem
---
+++ src/components/sections/DevelopmentStacks/index.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes} from 'react';
+import { connect } from 'react-redux';
+
+import stacksData from "../../../data/stacks"
+import ListItem from "./ListItem"
+
+class DevelopmentStacks extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-lg-12">
+            <h2 id="development-stacks">Development Stacks</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-4">
+            <ListItem
+              data={stacksData.backend}
+              handleSetFilters={this.props.handleSetFilters} />
+          </div>
+          <div className="col-lg-4">
+            <ListItem
+              data={stacksData.frontend}
+              handleSetFilters={this.props.handleSetFilters} />
+          </div>
+          <div className="col-lg-4">
+            <ListItem
+              data={stacksData.supports}
+              handleSetFilters={this.props.handleSetFilters} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = function(state, ownProps) {
+  const { filters } = state.sections
+  return {
+    filters
+  }
+}
+
+DevelopmentStacks.propTypes = {
+  handleSetFilters: PropTypes.func.isRequired
+}
+
+export default connect(mapStateToProps)(DevelopmentStacks);
