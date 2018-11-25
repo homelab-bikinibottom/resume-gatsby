e8bb40c0d3ad4ceaee07d6e08863b6cadb43b6ab
spondbob <spondbob@eamca.com>
2018-11-25 20:10:24 +1100

    publish

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from '../style';
+import MainComponent from './MainComponent';
+
+export default function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <MainComponent />
+    </div>
+  )
+}
