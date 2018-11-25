f17200342993476e05d261b33aea79ddb7316879
spondbob <spondbob@eamca.com>
2018-11-25 20:53:37 +1100

    handle empty section

--- src/components/sections/Section.js
+++ src/components/sections/Section.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import SectionItem from './SectionItem';
 
-class Section extends Component {
-  render() {
-    return (
-      <div>
+export default function Section({ data, id, title }) {
+  if ( data.length === 0 ) return null
+  return (
+    <div>
       <div className="row">
         <div className="col-lg-12">
-          <h2 id={this.props.id}>{this.props.title}</h2>
+          <h2 id={id}>{title}</h2>
         </div>
       </div>
       {
-        this.props.data.map(function(data, key) {
-          return (
-            <SectionItem data={data} key={key} />
-          );
-        })
+        data.map((data, key) => (
+          <SectionItem data={data} key={key} />
+        ))
       }
-      </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Section;
